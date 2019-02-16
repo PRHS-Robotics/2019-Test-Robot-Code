994c105f4539a577f9df772e332cc00cddac23da
Zachry Sheppard <zshepp@github.com>
2019-02-15 20:42:46 -0700

    Added documentation to Approach Tape

--- 2019-Test-Robot-Code/src/main/cpp/commands/ApproachTape.cpp
+++ 2019-Test-Robot-Code/src/main/cpp/commands/ApproachTape.cpp
@@ -16,22 +16,26 @@ ApproachTape::ApproachTape() :
 }
 
 void ApproachTape::Initialize() {
-	Robot::m_lights->UpdateDutyCycle(0.2);
+	Robot::m_lights->UpdateDutyCycle(0.2); //sets power of the led lights to 20 percent
 
-	auto ntinstance = nt::NetworkTableInstance::GetDefault();
-	auto table = ntinstance.GetTable("ChickenVision");
+	auto ntinstance = nt::NetworkTableInstance::GetDefault(); //receives data from rasberry pi networks
+	auto table = ntinstance.GetTable("ChickenVision"); // gets the Chicken Vision network from pi networks
 
-	nt::NetworkTableEntry useTape = table->GetEntry("Tape");
+	//checks to make sure the tape function in chicken vision is being used
+	nt::NetworkTableEntry useTape = table->GetEntry("Tape"); 
 	useTape.SetBoolean(true);
 }
 
 void ApproachTape::Execute() {
-	auto result = getTargetYaw();
+	auto result = getTargetYaw(); // sets result as the yaw value of the cargo
 
+	//sets the speed that the robot turns to follow the cargo
+	//sets the speed that the robot chases the cargo forward
 	double turningSpeed = result.first / 50.0;
 	double forwardSpeed = result.second * std::max((1.0 - std::abs(result.first / 10.0)) * 0.2, 0.0);
 
 	/* --- TODO: Determine distance --- */
+	//if the ultrasonic detects something the robot will stop itself
 	if (Robot::m_sonarMax->getDistance() < 25.0) {
 		turningSpeed = 0.0;
 		forwardSpeed = 0.0;
@@ -46,8 +50,8 @@ bool ApproachTape::IsFinished() {
 }
 
 void ApproachTape::End() {
-    Robot::m_driveTrain->drive(0.0, 0.0);
-	Robot::m_lights->UpdateDutyCycle(0.0);
+    Robot::m_driveTrain->drive(0.0, 0.0); //sets the speed of the robot to zero
+	Robot::m_lights->UpdateDutyCycle(0.0); // sets the power of the lights to zero
 }
 
 void ApproachTape::Interrupted() {
@@ -55,17 +59,17 @@ void ApproachTape::Interrupted() {
 }
 
 std::pair< double, bool > ApproachTape::getTargetYaw() {
-	auto ntinstance = nt::NetworkTableInstance::GetDefault();
-	auto table = ntinstance.GetTable("ChickenVision");
+	auto ntinstance = nt::NetworkTableInstance::GetDefault(); // retrieves network data from the rasberry pi
+	auto table = ntinstance.GetTable("ChickenVision"); //retrieves chicken vision table from rasberry pi 
 
-	nt::NetworkTableEntry detected = table->GetEntry("tapeDetected");
-	nt::NetworkTableEntry yaw = table->GetEntry("tapeYaw");
+	nt::NetworkTableEntry detected = table->GetEntry("tapeDetected"); // value of 1 if tape is detected: value of 0 if tape is not detected
+	nt::NetworkTableEntry yaw = table->GetEntry("tapeYaw"); //value for the yaw of the tape
 
-	frc::SmartDashboard::PutNumber("Tape Contours", table->GetEntry("tapeContours").GetDouble(0.0));
+	frc::SmartDashboard::PutNumber("Tape Contours", table->GetEntry("tapeContours").GetDouble(0.0)); //detects the number of pieces of tape in the frame of vision
 
 	if (!detected.GetBoolean(false)) {
-		return { 0.0, false };
+		return { 0.0, false }; // if no tape is detected then the returned value is false
 	}
 
-	return { yaw.GetDouble(0.0) - 5.0, true };
+	return { yaw.GetDouble(0.0) - 5.0, true }; // returns the postition of tape if tape is detected
 }
